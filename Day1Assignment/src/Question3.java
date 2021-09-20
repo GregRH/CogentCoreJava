3600efdf2cf5b41562bd12d9f92a572a920849df
gregoryhoag <gryanhoag@gmail.com>
2021-09-20 17:16:47 -0400

    question 3

--- Day1Assignment/src/Question3.java
+++ Day1Assignment/src/Question3.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 
 public class Question3 {
-
+	//No error handling
+	public static void main(String[] args) {
+		int nums[]=new int[4];
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the digits:");
+		for(int i=0;i<nums.length;i++)
+			nums[i]=input.nextInt();
+		for(int i =0;i<nums.length;i++) {
+			System.out.println(nums[i]+"-"+(char)nums[i]);
+		}
+	}
 }
